acc0b0c576bc219e762bccd4b7ef05ebc0d324a6
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-11-01 21:03:11 -0700

    started lab

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,33 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
   return (
-    <AppBar position="static" style={{ padding: '8px' }}>
-      <Toolbar>
-        <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>
-          Jonathan's Store
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <Box>
+      <AppBar position="static" style={{ padding: '8px' }}>
+        <Toolbar>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+            Jonathan's Store
+          </Typography>
+
+          <Button color="inherit">Cart</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
