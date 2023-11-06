2086ab25d91aa0fc9a709fb8f660a98677864776
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-11-05 22:45:51 -0800

    still need to complete part of lab

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,21 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import ProductList from './components/Products';
 import Categories from './components/Categories';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getProducts } from './store/products';
+import { getCategory } from './store/active-category';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const productData = useSelector((state) => state.products.productData);
+  useEffect(() => {
+    // console.log('useEffect');
+    dispatch(getProducts());
+    dispatch(getCategory());
+  }, [dispatch]);
+
+  // console.log(productData);
   return (
     <div>
       <Header />
--- src/store/products.js
+++ src/store/products.js
@@ -1,10 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
-import productData from '../data.json';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import productData from '../data.json';
+
+const url = import.meta.env.VITE_API_URL;
+
+export const getProducts = createAsyncThunk('GET/products', async () => {
+  // console.log('Fetching products from the API');
+  const response = await fetch(`${url}/products`);
+  const json = await response.json();
+  // console.log('Received product data:', json.results);
+  return json.results;
+});
+
+export const updateProduct = createAsyncThunk(
+  'PUT/product/:id',
+  async ({ product, amount }) => {
+    const updatedProduct = { ...product, inStock: product.inStock - amount };
+
+    const response = await fetch(`${url}/products/${product._id}`, {
+      method: 'PUT',
+      body: JSON.stringify(updatedProduct),
+      headers: { 'content-Type': 'application/json' },
+    });
+
+    const json = await response.json();
+    return json;
+  }
+);
 
 const productSlice = createSlice({
   name: 'products',
   initialState: {
-    productData: productData.products,
+    productData: [],
     selectedProduct: null,
     // price: 'all',
   },
@@ -12,9 +38,33 @@ const productSlice = createSlice({
     showProduct: (state, action) => {
       state.selectedProduct = action.payload;
     },
-    setCategory: (state, action) => {
-      state.setCategory = action.payload;
+    updateProduct: (state, action) => {
+      const { product, amount } = action.payload;
+      const updatedProduct = state.productData.find(
+        (p) => p._id === product._id
+      );
+
+      if (updatedProduct) {
+        updatedProduct.inStock += amount;
+      }
     },
+    setCategory: (state, action) => {
+      state.category = action.payload;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.productData = action.payload;
+      })
+      .addCase(updateProduct.fulfilled, (state, action) => {
+        const updatedProduct = action.payload;
+        const index = state.productData.findIndex(
+          (p) => p._id === updatedProduct._id
+        );
+        state.productData[index] = updatedProduct;
+      });
   },
 });
 
